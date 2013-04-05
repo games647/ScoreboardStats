b1e1feb6ba86e1c5d48cd57c2052989278732216
games647 <games647@games647-PC>
2013-04-05 22:18:43 +0200

    Memory improvement

--- Scoreboard/src/me/games647/scoreboardstats/ScoreboardStats.java
+++ Scoreboard/src/me/games647/scoreboardstats/ScoreboardStats.java
@@ -16,8 +16,8 @@ public final class ScoreboardStats extends org.bukkit.plugin.java.JavaPlugin {
     public void onEnable() {
         settings = new SettingsHandler(this);
         setupDatabase();
-        setupListeners();
-        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new me.games647.scoreboardstats.api.UpdateThread(), 60L, getSettings().getDelay() * 20L);
+        getServer().getPluginManager().registerEvents(new me.games647.scoreboardstats.listener.PlayerListener(), this);
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new me.games647.scoreboardstats.api.UpdateThread(), 60L, getConfig().getInt("Scoreboard.Update-delay") * 20L);
     }
 
     private void setupDatabase() {
@@ -30,7 +30,9 @@ public final class ScoreboardStats extends org.bukkit.plugin.java.JavaPlugin {
             getLogger().info("Can't find an existing Database, so creating a new one");
             installDDL();
         }
-        me.games647.scoreboardstats.pvpstats.Database.setDatabase(getDatabase());
+        me.games647.scoreboardstats.api.Database.setDatabase(getDatabase());
+        
+        getServer().getPluginManager().registerEvents(new me.games647.scoreboardstats.listener.EntityListener(), this);
     }
 
     @Override
@@ -40,12 +42,6 @@ public final class ScoreboardStats extends org.bukkit.plugin.java.JavaPlugin {
         list.add(PlayerStats.class);
 
         return list;
-    }
-
-    private void setupListeners() {
-        final org.bukkit.plugin.PluginManager pm = this.getServer().getPluginManager();
-        pm.registerEvents(new me.games647.scoreboardstats.listener.PlayerListener(), this);
-        pm.registerEvents(new me.games647.scoreboardstats.listener.EntityListener(), this);
     }
 
     @Override
