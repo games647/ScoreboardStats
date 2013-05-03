8015ef3439ef3b273aa29ee285afb22cf9da441f
games647 <games@tsprivat.de>
2013-05-03 13:39:10 +0200

    0.5.2
    Former-commit-id: f17cc0ea5d6f30b15c5421f3d43d63b8eedb3e1a [formerly dddf592c19d7d967ba0d2d4957c5a9538a9caea2]
    Former-commit-id: adf85d0bec6abcafe33b9a2a9eee04e3cb77c034

--- src/main/java/me/games647/scoreboardstats/api/pvpstats/TempScoreShow.java
+++ src/main/java/me/games647/scoreboardstats/api/pvpstats/TempScoreShow.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 public final class TempScoreShow implements Runnable {
 
     private final Player player;
+    private int tried;
 
     public TempScoreShow(final Player paramplayer) {
         this.player = paramplayer;
@@ -12,10 +13,10 @@ public final class TempScoreShow implements Runnable {
 
     @Override
     public void run() {
-        if (!player.isOnline()) {
+        if (!player.isOnline() || tried == 5) {
             return;
         }
-
+        
         Database.saveAccount(player.getName(), false);
         me.games647.scoreboardstats.api.Score.createTopListScoreboard(player);
     }
