4c5d2afc9bf4e7d440d6b446c0066a7f91824dd9
games647 <games@tsprivat.de>
2013-05-16 20:10:39 +0200

    Fix some Bugs and revert the PacketManager. Release it now as a different version.
    Former-commit-id: 0ea26aa800e5ea8d05dc5882ecba6158e23f50ec [formerly 0c98ac05766874a1f0bdfd3b9e55afd70c99fa16]
    Former-commit-id: b2abba01076fed7c368e7ebc8edc06a82ec490ae

--- src/main/java/com/github/games647/scoreboardstats/UpdateThread.java
+++ src/main/java/com/github/games647/scoreboardstats/UpdateThread.java
@@ -5,15 +5,6 @@ public final class UpdateThread implements Runnable {
     @Override
     public void run() {
         for (org.bukkit.entity.Player player : org.bukkit.Bukkit.getOnlinePlayers()) {
-            final org.bukkit.scoreboard.Objective objective = player.getScoreboard().getObjective(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
-
-            if (objective == null) {
-                com.github.games647.scoreboardstats.scoreboard.ScoreboardManager.createScoreboard(player);
-                continue;
-            } else if (!objective.getName().startsWith("ScoreboardStats")) {
-                continue;
-            }
-
             com.github.games647.scoreboardstats.ScoreboardStats.getSettings().sendUpdate(player, false);
         }
     }
