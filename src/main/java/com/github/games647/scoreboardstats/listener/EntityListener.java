4e0e669f5044116a1cc6bb044ef5ec6b8513fc9b
games647 <TS@212.238.61.96>
2013-05-19 19:53:01 +0200

    Little little cleanup
    Former-commit-id: 8531d817a2e80e0cda7f207af971b441387dc764 [formerly 0bae2f42ae057cef1a3a00dc790771b7f195d1d9]
    Former-commit-id: 589a8a71b8ea741994f17f9583fb8cc4b6a3f5a4

--- src/main/java/com/github/games647/scoreboardstats/listener/EntityListener.java
+++ src/main/java/com/github/games647/scoreboardstats/listener/EntityListener.java
@@ -1,9 +1,5 @@
 package com.github.games647.scoreboardstats.listener;
 
-import static com.github.games647.scoreboardstats.ScoreboardStats.getSettings;
-import com.github.games647.scoreboardstats.pvpstats.Cache;
-import com.github.games647.scoreboardstats.pvpstats.Database;
-
 public final class EntityListener implements org.bukkit.event.Listener {
 
     @org.bukkit.event.EventHandler
@@ -11,11 +7,12 @@ public final class EntityListener implements org.bukkit.event.Listener {
         final org.bukkit.entity.LivingEntity entity = event.getEntity();
         final org.bukkit.entity.Player killer = entity.getKiller();
 
-        if (getSettings().checkWorld(entity.getWorld().getName()) || entity.getType().equals(org.bukkit.entity.EntityType.PLAYER) || killer == null || !killer.isOnline()) {
+        if (com.github.games647.scoreboardstats.ScoreboardStats.getSettings().checkWorld(entity.getWorld().getName())
+                || entity.getType().equals(org.bukkit.entity.EntityType.PLAYER) || killer == null || !killer.isOnline()) {
             return;
         }
 
-        final Cache killercache = Database.getCache(killer.getName());
+        final com.github.games647.scoreboardstats.pvpstats.Cache killercache = com.github.games647.scoreboardstats.pvpstats.Database.getCache(killer.getName());
 
         if (killercache != null) {
             killercache.increaseMob();
