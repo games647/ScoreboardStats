252d58966c5571c81ae803752aa593c6c5ac5927
games647 <games@tsprivat.de>
2013-05-21 15:19:25 +0200

    Base for the comming commands

---
+++ src/main/java/com/github/games647/scoreboardstats/commands/StatsCommand.java
@@ -0,0 +1,13 @@
+package com.github.games647.scoreboardstats.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public final class StatsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
