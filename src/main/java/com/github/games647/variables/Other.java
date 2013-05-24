7711f388256c6dc6d16eb251abb883b92d18b748
games647 <games@tsprivat.de>
2013-05-24 23:45:29 +0200

    Fix build issues
    Former-commit-id: b675e55238fd30224f643631538960e630729ae0 [formerly eeec11d6e35b0279a35f06177613cc1c87a73721]
    Former-commit-id: 6656d3859407516d50c8719fbc1f580e768af323

--- src/main/java/com/github/games647/variables/Other.java
+++ src/main/java/com/github/games647/variables/Other.java
@@ -2,7 +2,7 @@ package com.github.games647.variables;
 
 public final class Other  {
 
-    public static final String  PLUGIN_NAME         = "MainClass";
+    public static final String  PLUGIN_NAME         = "ScoreboardStats";
     public static final String  TOPLIST             = "ScoreboardStatsT";
     public static final String  CONFIG_NAME         = "config.yml";
 
@@ -12,6 +12,10 @@ public final class Other  {
     public static final int     TICKS_INT           = 20;
     public static final String  EMPTY_CRITERA       = "dummy";
     public static final char    CHATCOLOR_CHAR      = '&';
+
+    //Sound
+    public static final float   VOLUME              = 50F;
+    public static final float   PITCH               = 1F;
 
     public static final int     SECONDS             = 60;
     public static final int     INTO_NEXT_SIZE      = 1024;
