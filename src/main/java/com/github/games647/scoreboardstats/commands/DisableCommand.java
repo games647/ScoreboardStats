2a05683106a958392a2455ae26619c78a04fd80a
games647 <games@tsprivat.de>
2013-05-26 13:39:52 +0200

    Toggle Command

--- src/main/java/com/github/games647/scoreboardstats/commands/DisableCommand.java
+++ src/main/java/com/github/games647/scoreboardstats/commands/DisableCommand.java
@@ -1,13 +1,40 @@
 package com.github.games647.scoreboardstats.commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import com.github.games647.scoreboardstats.ScoreboardStats;
+import com.github.games647.variables.Message;
+import com.github.games647.variables.Permissions;
+import org.bukkit.entity.Player;
 
-public final class DisableCommand implements CommandExecutor {
+public final class DisableCommand implements org.bukkit.command.CommandExecutor {
+
+    private final ScoreboardStats plugin;
+
+    public DisableCommand(ScoreboardStats instance) {
+        plugin = instance;
+    }
 
     @Override
-    public boolean onCommand(CommandSender cs, Command command, String label, String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean onCommand(org.bukkit.command.CommandSender cs, org.bukkit.command.Command cmd, String label, String[] args) {
+        if (!cs.hasPermission(Permissions.HIDE_PERMISSION)) {
+            cs.sendMessage(Message.PERMISSION_DENIED);
+            return true;
+        }
+
+        if (!(cs instanceof Player)) {
+            cs.sendMessage(Message.NO_CONSOLE);
+            return true;
+        }
+
+        final String                name = cs.getName();
+        final java.util.Set<String> list = plugin.hidelist;
+
+        if (list.contains(name)) {
+            list.remove(name);
+        } else {
+            list.add(name);
+        }
+
+        ((Player) cs).getScoreboard().clearSlot(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+        return true;
     }
 }
