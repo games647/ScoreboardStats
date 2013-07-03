e06ce85eb800b5fa07e29125e7731b6a755991ea
games647 <games@tsprivat.de>
2013-07-03 15:37:19 +0200

    Fix build issue
    Former-commit-id: 8e436e4a71bc237fdaa55811e813313a6775e07d [formerly 228d75a82943c71711d9c699fb20fa306cbac453]
    Former-commit-id: 2adae710ba6d2df75bf62627558eacc6709deff3

--- src/main/java/com/github/games647/scoreboardstats/scoreboard/SScore.java
+++ src/main/java/com/github/games647/scoreboardstats/scoreboard/SScore.java
@@ -1,17 +1,19 @@
 package com.github.games647.scoreboardstats.scoreboard;
 
+import net.minecraft.server.v1_6_R1.Packet207SetScoreboardScore;
+
 import org.apache.commons.lang.Validate;
 
 
 public final class SScore {
 
-    private String  displayname;
     private String  scorename;
+    private String  displayname;
 
-    private int     score   = Integer.MIN_VALUE;
+    private int     score       = Integer.MIN_VALUE;
     private boolean disabled;
 
-    public SScore(final String displayname, final String scorename, final int score) {
+    public SScore(String displayname, String scorename, int score) {
         Validate.isTrue(displayname.length() > 16, "The display name for the score can't be longer than 16 characters");
         Validate.isTrue(scorename.length() > 16, "The score name can't be longer than 16 characters");
 
@@ -20,7 +22,7 @@ public final class SScore {
         this.score          = score;
     }
 
-    public SScore(final String displayname, final int score) {
+    public SScore(String displayname, int score) {
         Validate.isTrue(displayname.length() > 16, "The display name for the score can't be longer than 16 characters");
 
         this.displayname    = displayname;
@@ -32,7 +34,7 @@ public final class SScore {
         return displayname;
     }
 
-    public void setDisplayname(final String displayname) {
+    public void setDisplayname(String displayname) {
         Validate.isTrue(displayname.length() > 16, "The display name for the score can't be longer than 16 characters");
         this.displayname = displayname;
     }
@@ -41,7 +43,7 @@ public final class SScore {
         return scorename;
     }
 
-    public void setScorename(final String scorename) {
+    public void setScorename(String scorename) {
         Validate.isTrue(scorename.length() > 16, "The score name can't be longer than 16 characters");
         this.scorename = scorename;
     }
@@ -50,7 +52,23 @@ public final class SScore {
         return score;
     }
 
-    public void setScore(final int score) {
+    public void setScore(int score) {
         this.score = score;
     }
+
+    public void setEnabled() {
+        final Packet207SetScoreboardScore packet = new Packet207SetScoreboardScore();
+
+        packet.a = scorename;
+        packet.b = displayname;
+        packet.d = score;
+    }
+//
+//    public void setDisabled() {
+//
+//    }
+//
+//    public void update() {
+//
+//    }
 }
