8934294eefe48192965a682df28e2893d7d1d035
games647 <games@tsprivat.de>
2013-07-16 21:09:53 +0200

    Update interfacename
    Former-commit-id: f8cdae2ed2fdd929029127b1bb7e5b8b2be0743e [formerly 43484063d46f4f98b62e77c475f27fc0d05fc5a8]
    Former-commit-id: 7731a6ff19d130df7043702399a939abababc4c3

--- src/main/java/com/github/games647/scoreboardstats/compatibility/Craftv1_6_R1.java
+++ src/main/java/com/github/games647/scoreboardstats/compatibility/Craftv1_6_R1.java
@@ -3,7 +3,7 @@ package com.github.games647.scoreboardstats.compatibility;
 import org.bukkit.craftbukkit.v1_6_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-public final class Craftv1_6_R1 implements ICraftPlayerPing {
+public final class Craftv1_6_R1 implements ICraftPlayerPingable {
 
     @Override
     public int getPlayerPing(Player player) {
--- src/main/java/com/github/games647/scoreboardstats/compatibility/Craftv1_6_R2.java
+++ src/main/java/com/github/games647/scoreboardstats/compatibility/Craftv1_6_R2.java
@@ -3,7 +3,7 @@ package com.github.games647.scoreboardstats.compatibility;
 import org.bukkit.craftbukkit.v1_6_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-public final class Craftv1_6_R2 implements ICraftPlayerPing {
+public final class Craftv1_6_R2 implements ICraftPlayerPingable {
 
     @Override
     public int getPlayerPing(Player player) {
