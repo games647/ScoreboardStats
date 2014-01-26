366efd0a375324fdec329fe548935a27b791550e
vemacs <gentoo@outlook.com>
2014-01-26 09:53:53 -0700

    add uSkyBlock dependency
    Supports adding the island level with the %island_level% variable, with
    the stable version of uSkyBlock (1.0.8)
    
    
    Former-commit-id: 55f5f32a939023a10e9dc242f926a7471f619167 [formerly 87add14131acf261cdc749697ebb3df652bfa65c]
    Former-commit-id: fd8e791c6299fdcbfc4c6e71fd4ab17012bd1494

---
+++ src/main/java/com/github/games647/scoreboardstats/variables/SkyBlockVariables.java
@@ -0,0 +1,37 @@
+package com.github.games647.scoreboardstats.variables;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import us.talabrek.ultimateskyblock.PlayerInfo;
+import us.talabrek.ultimateskyblock.uSkyBlock;
+
+public class SkyBlockVariables implements ReplaceManager.Replaceable {
+    private uSkyBlock instance;
+
+    public SkyBlockVariables() {
+        initialize();
+    }
+
+    @Override
+    public int getScoreValue(Player player, String variable) {
+        final PlayerInfo pi = instance.getActivePlayers().containsKey(player.getName()) ?
+                instance.getActivePlayers().get(player.getName()) :
+                instance.readPlayerFile(player.getName());
+        if ("%island_level%".equals(variable)) {
+            return pi == null ? 0 : pi.getIslandLevel();
+        }
+        return UNKOWN_VARIABLE;
+    }
+
+    private void initialize() {
+        final PluginManager pluginManager = Bukkit.getServer().getPluginManager();
+        final Plugin skyblockPlugin = pluginManager.getPlugin("uSkyBlock");
+        final int version = Integer.parseInt(skyblockPlugin.getDescription().getVersion().replace(".", ""));
+        if (version >= 200) {
+            throw new UnsupportedPluginException();
+        }
+        instance = uSkyBlock.getInstance();
+    }
+}
