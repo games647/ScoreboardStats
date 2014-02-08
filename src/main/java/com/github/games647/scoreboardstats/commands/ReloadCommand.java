1969e051735256a47653c5061d082c651ed0c317
games647 <games@tsprivat.de>
2014-02-08 15:46:04 +0100

    Don't double buffer the input

--- src/main/java/com/github/games647/scoreboardstats/commands/ReloadCommand.java
+++ src/main/java/com/github/games647/scoreboardstats/commands/ReloadCommand.java
@@ -1,6 +1,6 @@
 package com.github.games647.scoreboardstats.commands;
 
-import com.github.games647.scoreboardstats.Language;
+import com.github.games647.scoreboardstats.Lang;
 import com.github.games647.scoreboardstats.ScoreboardStats;
 
 import org.bukkit.command.Command;
@@ -15,12 +15,12 @@ public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command cmd, final String label, final String[] args) {
         if (!commandSender.hasPermission("scoreboardstats.reload")) {
-            commandSender.sendMessage(Language.get("noPermission"));
+            commandSender.sendMessage(Lang.get("noPermission"));
             return true;
         }
 
         ScoreboardStats.getInstance().onReload();
-        commandSender.sendMessage(Language.get("onReload"));
+        commandSender.sendMessage(Lang.get("onReload"));
         return true;
     }
 }
