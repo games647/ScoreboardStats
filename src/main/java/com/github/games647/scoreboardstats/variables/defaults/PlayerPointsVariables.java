2e2c157e1170dfb428da242d928fd38baccd59a9
games647 <games@tsprivat.de>
2015-04-02 16:42:25 +0200

    Fix PlayerPoints support

--- src/main/java/com/github/games647/scoreboardstats/variables/defaults/PlayerPointsVariables.java
+++ src/main/java/com/github/games647/scoreboardstats/variables/defaults/PlayerPointsVariables.java
@@ -5,7 +5,8 @@ import com.github.games647.scoreboardstats.variables.ReplaceManager;
 import com.github.games647.scoreboardstats.variables.VariableReplaceAdapter;
 
 import org.black_ixx.playerpoints.PlayerPoints;
-import org.black_ixx.playerpoints.event.PlayerPointsEvent;
+import org.black_ixx.playerpoints.event.PlayerPointsChangeEvent;
+import org.black_ixx.playerpoints.event.PlayerPointsResetEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +14,8 @@ import org.bukkit.event.Listener;
 
 /**
  * Represents a replacer for the Plugin PlayerPoints
+ *
+ * http://dev.bukkit.org/bukkit-plugins/playerpoints/
  */
 public class PlayerPointsVariables extends VariableReplaceAdapter<PlayerPoints> implements Listener {
 
@@ -32,7 +35,15 @@ public class PlayerPointsVariables extends VariableReplaceAdapter<PlayerPoints> 
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onPointsChange(PlayerPointsEvent changeEvent) {
+    public void onPointsReset(PlayerPointsResetEvent resetEvent) {
+        final Player player = Bukkit.getPlayer(resetEvent.getPlayerId());
+        if (player != null) {
+            replaceManager.updateScore(player, "points", 0);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPoints(PlayerPointsChangeEvent changeEvent) {
         final Player player = Bukkit.getPlayer(changeEvent.getPlayerId());
         if (player != null) {
             final int lastBal = getPlugin().getAPI().look(changeEvent.getPlayerId());
