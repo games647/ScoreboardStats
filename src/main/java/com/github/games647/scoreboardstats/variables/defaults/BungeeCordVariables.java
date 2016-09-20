60a97109dceada049d8280388e58de3206818757
games647 <games@tsprivat.de>
2016-09-20 20:01:52 +0200

    Add server specific bungee online variable

--- src/main/java/com/github/games647/scoreboardstats/variables/defaults/BungeeCordVariables.java
+++ src/main/java/com/github/games647/scoreboardstats/variables/defaults/BungeeCordVariables.java
@@ -4,9 +4,12 @@ import com.github.games647.scoreboardstats.BackwardsCompatibleUtil;
 import com.github.games647.scoreboardstats.variables.ReplaceEvent;
 import com.github.games647.scoreboardstats.variables.ReplaceManager;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 
@@ -20,21 +23,30 @@ public class BungeeCordVariables extends DefaultReplaceAdapter<Plugin> implement
     private static final String BUNGEE_CHANNEL = "BungeeCord";
 
     private final ReplaceManager replaceManager;
-    private int playersCount;
+    private Map<String, Integer> playersCount = Maps.newHashMap();
 
     public BungeeCordVariables(ReplaceManager replaceManager) {
-        super(Bukkit.getPluginManager().getPlugin("ScoreboardStats"), "bungee-online");
+        super(null, "", true, false, true, "bungee-online", "bungee_*");
 
         this.replaceManager = replaceManager;
         Bukkit.getMessenger().registerOutgoingPluginChannel(getPlugin(), BUNGEE_CHANNEL);
         Bukkit.getMessenger().registerIncomingPluginChannel(getPlugin(), BUNGEE_CHANNEL, this);
 
         Bukkit.getScheduler().runTaskTimer(getPlugin(), this, UPDATE_INTERVAL, UPDATE_INTERVAL);
+        playersCount.put("ALL", -1);
     }
 
     @Override
     public void onReplace(Player player, String variable, ReplaceEvent replaceEvent) {
-        replaceEvent.setScoreOrText(playersCount);
+        if ("bungee-online".equals(variable)) {
+            replaceEvent.setScoreOrText(playersCount.get("ALL"));
+        } else {
+            String serverName = variable.replace("bungee_", "");
+            int count = playersCount.computeIfAbsent(serverName, key -> -1);
+            replaceEvent.setScoreOrText(count);
+        }
+
+        
         replaceEvent.setConstant(true);
     }
 
@@ -48,21 +60,28 @@ public class BungeeCordVariables extends DefaultReplaceAdapter<Plugin> implement
         String subchannel = in.readUTF();
         if ("PlayerCount".equals(subchannel)) {
             String server = in.readUTF();
+            int count = in.readInt();
             //update variable for cache
-            playersCount = in.readInt();
-            replaceManager.updateScore("bungee-online", playersCount);
+            playersCount.put(server, count);
+
+            if ("ALL".equals(server)) {
+                replaceManager.updateScore("bungee-online", count);
+            } else {
+                replaceManager.updateScore("bungee_" + server, count);
+            }
         }
     }
 
     @Override
     public void run() {
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF("PlayerCount");
-        out.writeUTF("ALL");
-
         Player sender = Iterables.getFirst(BackwardsCompatibleUtil.getOnlinePlayers(), null);
         if (sender != null) {
-            sender.sendPluginMessage(getPlugin(), BUNGEE_CHANNEL, out.toByteArray());
+            for (String serverName : playersCount.keySet()) {
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("PlayerCount");
+                out.writeUTF(serverName);
+                sender.sendPluginMessage(getPlugin(), BUNGEE_CHANNEL, out.toByteArray());
+            }
         }
     }
 }
