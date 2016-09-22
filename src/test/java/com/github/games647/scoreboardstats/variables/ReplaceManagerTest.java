f6e2de338c78c585d0e28a45aec0680850e76dfc
games647 <games@tsprivat.de>
2016-09-22 13:14:14 +0200

    Fix tests

--- src/test/java/com/github/games647/scoreboardstats/variables/ReplaceManagerTest.java
+++ src/test/java/com/github/games647/scoreboardstats/variables/ReplaceManagerTest.java
@@ -54,8 +54,11 @@ public class ReplaceManagerTest {
 
     @SuppressWarnings("deprecation")
     private void testLegacy(ReplaceManager replaceManager) {
-        Replaceable legacyReplaceable = (Player player, String variable) -> {
-            throw new UnsupportedOperationException("Not supported yet");
+        Replaceable legacyReplaceable = new Replaceable() {
+            @Override
+            public int getScoreValue(Player player, String variable) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
         };
 
         LegacyReplaceWrapper legacyWrapper = new LegacyReplaceWrapper(Bukkit
